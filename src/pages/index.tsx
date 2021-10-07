cef0bbe6396a8a6db33f54805337af8145144417
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-10-07 00:10:59 -0300

    Estilos Globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello World 👋
       </h1>
     </>
